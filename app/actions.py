a8ab64f2454f7a71dc68e085439fbebced0e137d
Vadim Melchaev <104781245+meelovar@users.noreply.github.com>
2022-06-13 18:54:24 +0000

    Add create and edit operations for all packs except permission

--- app/actions.py
+++ app/actions.py
@@ -1,9 +1,14 @@
+import objectpack.ui
 from django.contrib.auth import models
 from objectpack.actions import ObjectPack
+
+from .ui import UserAddWindow
 
 
 class ContentTypePack(ObjectPack):
     model = models.ContentType
+
+    add_window = edit_window = objectpack.ui.ModelEditWindow.fabricate(model)
 
     add_to_desktop = True
     add_to_menu = True
@@ -12,12 +17,17 @@ class ContentTypePack(ObjectPack):
 class UserPack(ObjectPack):
     model = models.User
 
+    add_window = edit_window = UserAddWindow
+    # add_window = edit_window = objectpack.ui.ModelEditWindow.fabricate(model)
+
     add_to_desktop = True
     add_to_menu = True
 
 
 class GroupPack(ObjectPack):
     model = models.Group
+
+    add_window = edit_window = objectpack.ui.ModelEditWindow.fabricate(model)
 
     add_to_desktop = True
     add_to_menu = True

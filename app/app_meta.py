9f8de865419615bf69f3a36cb78965ec7346ec6d
Vadim Melchaev <104781245+meelovar@users.noreply.github.com>
2022-06-13 08:06:43 +0000

    Additional files to app

---
+++ app/app_meta.py
@@ -0,0 +1,26 @@
+from django.conf.urls import url
+from objectpack import desktop
+from .controller import controller
+
+
+def register_urlpatterns():
+    """
+    Регистрация конфигурации урлов для приложения
+    """
+    return [url(*controller.urlpattern)]
+
+
+def register_actions():
+    """
+    Регистрация экшен-паков
+    """
+    return controller.packs.extend([
+        # YourActionPack()
+    ])
+
+
+def register_desktop_menu():
+    """
+    Регистрация элементов рабочего стола
+    """
+    return desktop.uificate_the_controller(controller, menu_root=desktop.MainMenu.SubMenu("Demo"))

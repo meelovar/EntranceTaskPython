3b6531fadbc0e3325e62b9021b582453ee9630d9
Vadim Melchaev <104781245+meelovar@users.noreply.github.com>
2022-06-13 09:42:15 +0000

    Add m3 desktop to urls

--- m3_project/urls.py
+++ m3_project/urls.py
@@ -13,9 +13,22 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path
+from django.shortcuts import render
+
+from m3 import get_app_urlpatterns
+
+
+def workspace(request):
+    return render(request, "m3_workspace.html", context={"debug": settings.DEBUG})
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r"^$", workspace)
 ]
+
+urlpatterns.extend(get_app_urlpatterns())

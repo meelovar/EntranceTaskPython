545008a81081fff819f71665de779d31097a1ac4
Vadim Melchaev <104781245+meelovar@users.noreply.github.com>
2022-06-14 06:13:36 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="m3_project",
+    version="1.0",
+    packages=find_packages(include=("app", "m3_project")),
+    install_requires=(
+        "django==2.2",
+        "m3-django-compat==1.9.2",
+        "m3-objectpack==2.2.47"
+    )
+)

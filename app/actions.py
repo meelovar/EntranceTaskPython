dd79068baa2bf90b6c0257d36a35a276675d2050
Vadim Melchaev <104781245+meelovar@users.noreply.github.com>
2022-06-14 06:40:06 +0000

    Add columns to object packs

--- app/actions.py
+++ app/actions.py
@@ -9,6 +9,17 @@ from .ui import UserAddWindow
 class ContentTypePack(ObjectPack):
     model = models.ContentType
 
+    columns = (
+        {
+            "data_index": "app_label",
+            "header": "App label"
+        },
+        {
+            "data_index": "model",
+            "header": "Model class name"
+        }
+    )
+
     add_window = edit_window = objectpack.ui.ModelEditWindow.fabricate(model)
 
     add_to_desktop = True
@@ -17,6 +28,29 @@ class ContentTypePack(ObjectPack):
 
 class UserPack(ObjectPack):
     model = models.User
+
+    columns = (
+        {
+            "data_index": "username",
+            "header": "Username",
+            "width": 2
+        },
+        {
+            "data_index": "is_superuser",
+            "header": "Is superuser",
+            "width": 1
+        },
+        {
+            "data_index": "is_staff",
+            "header": "Is staff",
+            "width": 1
+        },
+        {
+            "data_index": "is_active",
+            "header": "Is active",
+            "width": 1
+        }
+    )
 
     add_window = edit_window = UserAddWindow
     # add_window = edit_window = objectpack.ui.ModelEditWindow.fabricate(model)
@@ -28,6 +62,13 @@ class UserPack(ObjectPack):
 class GroupPack(ObjectPack):
     model = models.Group
 
+    columns = (
+        {
+            "data_index": "name",
+            "header": "Name"
+        },
+    )
+
     add_window = edit_window = objectpack.ui.ModelEditWindow.fabricate(model)
 
     add_to_desktop = True
@@ -36,6 +77,21 @@ class GroupPack(ObjectPack):
 
 class PermissionPack(ObjectPack):
     model = models.Permission
+
+    columns = (
+        {
+            "data_index": "name",
+            "header": "Name"
+        },
+        {
+            "data_index": "codename",
+            "header": "Codename"
+        },
+        {
+            "data_index": "content_type",
+            "header": "Content type"
+        }
+    )
 
     add_window = edit_window = objectpack.ui.ModelEditWindow.fabricate(model, model_register=observer)
     add_to_desktop = True
